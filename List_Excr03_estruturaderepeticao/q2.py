80855bcb99b2b67e14da250f829fe0421353b6ba
Satur <82034427+Satur696@users.noreply.github.com>
2022-09-19 19:53:30 -0300

    Delete List_Excr03_estruturaderepeticao directory

--- List_Excr03_estruturaderepeticao/q2.py
+++
@@ -1,8 +0,0 @@
-n = 0
-soma = 0
-
-while n < 500:
-    if n%2 == 1:
-        soma = n+soma
-    n += 1
-print(f"A soma dos número impáres de 1 a 500 é: {soma}")
--- List_Excr03_estruturaderepeticao/q3.py
+++
@@ -1,7 +0,0 @@
-numero = 100
-
-print("Os número impáres entre 100 e 200 são:")
-while numero < 200:
-    if numero%2==1:
-        print(numero)
-    numero +=1
